9689cf0cdbc8a80040d0ee7ffe2bb82612356c29
Miika Piiparinen <100348027+MiikaMatias@users.noreply.github.com>
2024-07-27 18:13:54 +0300

    2.03 and 2.04: namespace sep

--- reinstall.sh
+++ reinstall.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 PROJECT_PATH=/home/miika/Projects/devopsK8s
 PROJECT_DIRS=(
   log-output
@@ -20,3 +21,33 @@ done
 cd /home/miika/Projects/devopsK8s
 
 kubectl apply -f manifests/
+=======
+source .env
+cd ${HOME_PATH}
+
+kubectl config set-context --current --namespace=applications
+bash uninstall.sh
+kubectl delete all --all            
+docker rmi $(docker images -q)
+kubectl apply -f manifests/application-manifests/
+
+kubectl config set-context --current --namespace=project
+bash uninstall.sh
+kubectl delete all --all            
+docker rmi $(docker images -q)
+kubectl apply -f manifests/project-manifests/
+
+kubectl config set-context --current --namespace=applications
+for dir in "${PROJECT_DIRS[@]}"; do
+  cd "${HOME_PATH}/$dir" || exit
+  bash deploy.sh
+done
+
+kubectl config set-context --current --namespace=project
+for dir in "${APPLICATION_DIRS[@]}"; do
+  cd "${HOME_PATH}/$dir" || exit
+  bash deploy.sh
+done
+
+cd /home/miika/Projects/devopsK8s
+>>>>>>> 18d42f1 (2.03 and 2.04: namespace sep)
--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-
 source .env
+cd ${HOME_PATH}
 
 kubectl delete -f manifests/
 kubectl delete -f persistent-volumes/
 
-for dir in "${DIRS[@]}"; do
-  cd "$dir" || exit
+kubectl config set-context --current --namespace=applications
+for dir in "${PROJECT_DIRS[@]}"; do
+  cd "${HOME_PATH}/$dir" || exit
+  kubectl delete -f manifests/
+done
+
+kubectl config set-context --current --namespace=project
+for dir in "${APPLICATION_DIRS[@]}"; do
+  cd "${HOME_PATH}/$dir" || exit
   kubectl delete -f manifests/
 done
