49e8cf081676a15b7bcb93e3b0fd8580ba83ac6b
Miika Piiparinen <100348027+MiikaMatias@users.noreply.github.com>
2024-07-27 18:13:54 +0300

    2.03 and 2.04: namespace sep

--- reinstall.sh
+++ reinstall.sh
@@ -1,29 +1,5 @@
-<<<<<<< HEAD
-PROJECT_PATH=/home/miika/Projects/devopsK8s
-PROJECT_DIRS=(
-  log-output
-  ping-pong
-  to-do-backend
-  project
-)
-cd ${PROJECT_PATH}
 
-bash uninstall.sh
-kubectl delete all --all            
-docker rmi $(docker images -q)
-kubectl apply -f persistent-volumes/    
-
-for dir in "${PROJECT_DIRS[@]}"; do
-  cd "${PROJECT_PATH}/$dir" || exit
-  bash deploy.sh
-done
-
-cd /home/miika/Projects/devopsK8s
-
-kubectl apply -f manifests/
-=======
 source .env
-cd ${HOME_PATH}
 
 kubectl config set-context --current --namespace=applications
 bash uninstall.sh
@@ -49,5 +25,4 @@ for dir in "${APPLICATION_DIRS[@]}"; do
   bash deploy.sh
 done
 
-cd /home/miika/Projects/devopsK8s
->>>>>>> 18d42f1 (2.03 and 2.04: namespace sep)
+cd ${HOME_PATH}
